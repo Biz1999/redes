95070b3542a31da3271802b2d3747a2dad879433
lucas silva oliveira <lucassioliveira098@gmail.com>
2021-03-15 23:03:58 -0300

    create lab2 files

---
+++ lab2/Exercicio1/Client.py
@@ -0,0 +1,14 @@
+from socket import *
+serverName = "127.0.0.1"
+#cria socket TCP, porta
+#remota # 12000
+serverPort = 12000
+clientSocket = socket(AF_INET, SOCK_STREAM)
+clientSocket.connect((serverName,serverPort))
+sentence = input("Digite algo em letra minúscula:")
+#Envia a mensagem
+sen = sentence.encode()#formata a mensagem
+clientSocket.send(sen)#chama encode para mandar 
+modifiedSentence = clientSocket.recv(1024)
+print("From Server:", modifiedSentence.decode())
+clientSocket.close()
---
+++ lab2/Exercicio1/Server.py
@@ -0,0 +1,40 @@
+#esqueleto de um servidor HTTP/1.1
+
+from socket import *
+
+#define qual o so   cket irá escutar
+serverPort = 12000
+
+#cria o socket
+serverSocket = socket(AF_INET, SOCK_STREAM)
+
+#atribuir o socket a uma porta especifica
+serverSocket.bind(("", serverPort))
+
+#inicia o "listening"
+serverSocket.listen(1)
+
+print("Servidor HTTP/1.1 Inicializado")
+
+#loop principal que recebe as solicitações dos clientes
+while True:
+	connectionSocket, addr = serverSocket.accept()
+	print("Cliente {} conectado ao servidor".format(addr))
+	#recebe solicitação vinda do cliente
+	request = connectionSocket.recv(1024).decode()
+	#quebra cada palavra da requisição
+	split_request = request.split(sep=" ")
+	if split_request[0] == "GET":
+		#realiza ação pertinente ao comando GET
+		params = ' '.join(split_request[1:])
+		print("Solicitação do tipo GET, buscando o recurso {}".format(params))
+		#supondo que a solicitação foi de sucesso
+		response = (params.upper()).encode()
+		connectionSocket.send(response)
+	else:
+		#imprimo um erro no servidor
+		print("Comando não pode ser interpretado por esse servidor!")
+		#crio uma mensagem de erro e envio ao cliente
+		response = ("ERRO! Servidor não reconhece esse comando!").encode()
+		connectionSocket.send(response)
+	connectionSocket.close()
