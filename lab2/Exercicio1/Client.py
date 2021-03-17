fe7619669335272dbd74407be24f6cd693b514fa
Alessandro <alessanbiz@gmail.com>
2021-03-17 11:35:37 -0300

    while true
    while true in both files

--- lab2/Exercicio1/Client.py
+++ lab2/Exercicio1/Client.py
@@ -1,14 +1,22 @@
 from socket import *
-serverName = "127.0.0.1"
+serverName = 'localhost'
 #cria socket TCP, porta
 #remota # 12000
 serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName,serverPort))
-sentence = input("Digite algo em letra minúscula:")
-#Envia a mensagem
-sen = sentence.encode()#formata a mensagem
-clientSocket.send(sen)#chama encode para mandar 
-modifiedSentence = clientSocket.recv(1024)
-print("From Server:", modifiedSentence.decode())
+
+while True:
+    sentence = input("Digite algo em letra minúscula: ")
+    #Envia a mensagem
+    sen = sentence.encode()#formata a mensagem
+    clientSocket.send(sen)#chama encode para mandar 
+    modifiedSentence = clientSocket.recv(1024).decode()
+    print("From Server:", modifiedSentence)
+    sentenceVerif = sentence.split()
+    sentenceVerif = ' '.join(sentenceVerif[1:]).upper()
+    if modifiedSentence != sentenceVerif:
+        break
+
 clientSocket.close()
+
--- lab2/Exercicio1/Server.py
+++ lab2/Exercicio1/Server.py
@@ -16,25 +16,30 @@ serverSocket.listen(1)
 
 print("Servidor HTTP/1.1 Inicializado")
 
+
+
 #loop principal que recebe as solicitações dos clientes
 while True:
 	connectionSocket, addr = serverSocket.accept()
 	print("Cliente {} conectado ao servidor".format(addr))
-	#recebe solicitação vinda do cliente
-	request = connectionSocket.recv(1024).decode()
-	#quebra cada palavra da requisição
-	split_request = request.split(sep=" ")
-	if split_request[0] == "GET":
-		#realiza ação pertinente ao comando GET
-		params = ' '.join(split_request[1:])
-		print("Solicitação do tipo GET, buscando o recurso {}".format(params))
-		#supondo que a solicitação foi de sucesso
-		response = (params.upper()).encode()
-		connectionSocket.send(response)
-	else:
-		#imprimo um erro no servidor
-		print("Comando não pode ser interpretado por esse servidor!")
-		#crio uma mensagem de erro e envio ao cliente
-		response = ("ERRO! Servidor não reconhece esse comando!").encode()
-		connectionSocket.send(response)
+	while True:
+		#recebe solicitação vinda do cliente
+		request = connectionSocket.recv(1024).decode()
+		#quebra cada palavra da requisição
+		split_request = request.split()
+		if split_request[0] == "GET":
+			#realiza ação pertinente ao comando GET
+			params = ' '.join(split_request[1:])
+			print("Solicitação do tipo GET, buscando o recurso {}".format(params))
+			#supondo que a solicitação foi de sucesso
+			response = (params.upper()).encode()
+			connectionSocket.send(response)
+		else:
+			#imprimo um erro no servidor
+			print("Comando não pode ser interpretado por esse servidor!")
+			#crio uma mensagem de erro e envio ao cliente
+			response = ("ERRO! Servidor não reconhece esse comando!").encode()
+			connectionSocket.send(response)
+			break
+			
 	connectionSocket.close()
