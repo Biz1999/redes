7b4f84b0177aa90d30498fdddda9c9aebd2a2b86
Alessandro <alessanbiz@gmail.com>
2021-03-18 15:27:24 -0300

    Loop and DateTime
    Loop between client and server, and datetime feature

--- lab2/Exercicio1/Client.py
+++ lab2/Exercicio1/Client.py
@@ -3,19 +3,21 @@ serverName = 'localhost'
 #cria socket TCP, porta
 #remota # 12000
 serverPort = 12000
+
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName,serverPort))
 
 while True:
-    sentence = input("Digite algo em letra minúscula: ")
+    sentence = input("Pergunte algo à Alex: ")
     #Envia a mensagem
     sen = sentence.encode()#formata a mensagem
     clientSocket.send(sen)#chama encode para mandar 
     modifiedSentence = clientSocket.recv(1024).decode()
-    print("From Server:", modifiedSentence)
-    sentenceVerif = sentence.split()
-    sentenceVerif = ' '.join(sentenceVerif[1:]).upper()
-    if modifiedSentence != sentenceVerif:
+    print("Alex:", modifiedSentence)
+    errorDetection = modifiedSentence.split()
+    print(errorDetection)
+    j = True if "ERRO" in errorDetection else False
+    if j != False:
         break
 
 clientSocket.close()
--- lab2/Exercicio1/Server.py
+++ lab2/Exercicio1/Server.py
@@ -1,6 +1,7 @@
 #esqueleto de um servidor HTTP/1.1
 
 from socket import *
+from datetime import *
 
 #define qual o so   cket irá escutar
 serverPort = 12000
@@ -26,19 +27,35 @@ while True:
 		#recebe solicitação vinda do cliente
 		request = connectionSocket.recv(1024).decode()
 		#quebra cada palavra da requisição
-		split_request = request.split()
-		if split_request[0] == "GET":
-			#realiza ação pertinente ao comando GET
-			params = ' '.join(split_request[1:])
-			print("Solicitação do tipo GET, buscando o recurso {}".format(params))
-			#supondo que a solicitação foi de sucesso
-			response = (params.upper()).encode()
-			connectionSocket.send(response)
+		clientDoubt = request.split()
+		if clientDoubt[0] == "Alex":
+			j=0
+			for i in range(1,len(clientDoubt)):
+				if clientDoubt[i]=='ola' or clientDoubt[i]=='Ola' or clientDoubt[i]=='olá' or clientDoubt[i]=='Olá' :
+					response = ('Olá! Tudo bem?').encode()
+					connectionSocket.send(response)
+					j+=1
+				elif clientDoubt[i]=='tudo' or clientDoubt[i]=='Tudo' :
+					response = ('Ótimo!').encode()
+					connectionSocket.send(response)
+					j+=1
+				elif clientDoubt[i]=='horas' or clientDoubt[i]=='horário' or clientDoubt[i]=='horario':
+					horario = "São exatamente " + str( datetime.now().strftime('%H:%M'))
+					response = ( horario ).encode()
+					connectionSocket.send(response)
+					j+=1
+			
+			if j==0:
+				print("Comando não pode ser interpretado por esse servidor!")
+				#crio uma mensagem de erro e envio ao cliente
+				response = ("ERRO ! Alex não reconhece esse comando! Abra nova conexão").encode()
+				connectionSocket.send(response)
+				break
 		else:
 			#imprimo um erro no servidor
 			print("Comando não pode ser interpretado por esse servidor!")
 			#crio uma mensagem de erro e envio ao cliente
-			response = ("ERRO! Servidor não reconhece esse comando!").encode()
+			response = ("ERRO ! Alex não reconhece esse comando! Abra nova conexão").encode()
 			connectionSocket.send(response)
 			break
 			
