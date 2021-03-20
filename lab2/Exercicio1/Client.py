453540be83cab374c496475669e1e89aa7643858
Islan Silva <islan.silva28@gmail.com>
2021-03-20 19:14:40 -0300

    Inserção do usuario Windows
    Foi capturado o usuario logado do Windows e com isso, é feito o tratamento com esse usuário

--- lab2/Exercicio1/Client.py
+++ lab2/Exercicio1/Client.py
@@ -1,11 +1,34 @@
 from socket import *
+import getpass
 serverName = 'localhost'
+
 #cria socket TCP, porta
 #remota # 12000
 serverPort = 12000
 
+#captura usuariolog
+user_log = getpass.getuser()
+
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName,serverPort))
+
+#Parte que realiza o primeiro contato com o servidor, passando usuário logado
+#O Servidor vai questionar se o cliente deseja ser chamado pelo login ou por outro nome
+
+user_log = user_log.encode() #Formata o usuario logado
+clientSocket.send(user_log) #Envia o usuário para o servidor
+
+mensagemBoasVindas = clientSocket.recv(1024).decode()
+print("Alex:", mensagemBoasVindas)
+
+opcao = input("1 para manter seu nome ou digite um novo nome: ")
+opcao = opcao.encode()
+clientSocket.send(opcao)
+
+#Recebe instruções de como deve ser o tratamento
+mensagemBoasVindas = clientSocket.recv(1024).decode()
+print("Alex:", mensagemBoasVindas)
+
 
 while True:
     sentence = input("Pergunte algo à Alex: ")
