f8a2c45056efeefac775d05648a03256d757d564
Alessandro <alessanbiz@gmail.com>
2021-04-19 19:10:46 -0300

    HTTP server - post method
    Implementation of the post method on the http server

--- Projeto 1/servidorHTTP.py
+++ Projeto 1/servidorHTTP.py
@@ -4,38 +4,40 @@ import sys
 import codecs
 from datetime import datetime
 
+def sendPage( nome, error ):
 
-# data e horário
-date = (f'{datetime.now():%m-%Y %H:%M}')
+    # data e horário
+    date = (f'{datetime.now():%m-%Y %H:%M}')
 
-nome = ''
-
-# codigo da página
-page = f'''<!DOCTYPE HTML>\n<html> 
+    #código da página
+    page = f'''<!DOCTYPE HTML>\n<html> 
         <body>
             <h1>This is THE WEB PAGE</h1> 
             <br>
             Hello World!
             <form method='POST'>
                 <label style='background-color:red'>Primeiro nome:</label><br>
-                <input type="text" id="fname"><br>
-                <label for="lname">Sobrenome:</label><br>
-                <input type="text" id="lname"><br><br>
+                <input type="text" name="fname"><br>
+                <label>Sobrenome:</label><br>
+                <input type="text" name="lname"><br><br>
                 <input type="submit" value="Submit">
             </form> 
             <br>{date}<br>
             {nome}
         </body>
         </html>\r\n\r\n'''
-
-
-page404 = '''<!DOCTYPE HTML>\n<html>
+    page404 = '''<!DOCTYPE HTML>\n<html>
         <body>
             <h1>
             ERRO 404<br>Pagina nao encontrada
             </h1>
         </body>
         </html>\r\n\r\n'''
+    return (page if error==0 else page404)
+
+
+
+
 
 currentTime = datetime.now()
 d = currentTime.ctime()
@@ -76,7 +78,6 @@ while True:
             params = request[4]
         print(request)
 
-        #assuming the request was successful
         if params == f'localhost:{serverPort}':
             response = "\nHTTP/1.1 200 OK\r\n"
         else:
@@ -85,19 +86,27 @@ while True:
         response += "Connection: Keep-Alive\r\n"
         response += "Content-Type:text/html; charset=UTF-8\r\n\r\n"
         if params == f'localhost:{serverPort}':
-            response += page
+            response += sendPage('', 0)
         else:
-            response += page404
-        print(response)
+            response += sendPage('', 1)
         connectionSocket.send(response.encode())
 
     if request[0] == "POST":
+
         params = request[1]
         if request[1] == '/' and len(request) > 4:
             params = request[4]
+            
         print(request)
+        params2 = request[-1]
 
-        #assuming the request was successful
+        params_post = params2.split("&")
+        params_fname = params_post[0][params2.find('=') + 1:]
+        params_lname = params_post[1][params2.find('=') + 1:]
+
+        nome = f'{params_fname} {params_lname}'
+
+        print("Post type request, posting to {}".format(params))
         if params == f'localhost:{serverPort}':
             response = "\nHTTP/1.1 200 OK\r\n"
         else:
@@ -106,8 +115,8 @@ while True:
         response += "Connection: Keep-Alive\r\n"
         response += "Content-Type:text/html; charset=UTF-8\r\n\r\n"
         if params == f'localhost:{serverPort}':
-            response += page
+            response += sendPage(nome, 0)
         else:
-            response += page404
+            response += sendPage('', 1)
         connectionSocket.send(response.encode())
 
